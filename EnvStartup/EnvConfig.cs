42b57088da67f23a06672c5a51e7d043393d2d29
AmandaChou <AmandaChou@itsower.com.tw>
2023-05-13 14:31:15 +0800

    init

---
+++ EnvStartup/EnvConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace EnvStartup;
+public class EnvConfig
+{
+    public EnvConfig() { }
+    public EnvConfig(string dir)
+    {
+        var projectName = $@"D:/{dir.Split("/").Last()}";
+        ProjectDirectory = dir;
+        ProjectLogDirectory = $@"D:/{projectName}Info";
+        ProjectLogDirectory = $@"D:/{projectName}Log";
+    }
+    public string ProjectDirectory { get; set; }
+    public string ProjectInfoDirectory { get; set; }
+    public string ProjectLogDirectory { get; set; }
+    public string ServerEnvironment { get; set; } = "prod";
+}
---
+++ EnvStartup/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace EnvStartup;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        var dir = args[0];
+        var configName = "EnvConfig.json";
+        var configPath = Path.Combine(dir, configName);
+
+        if (!Directory.Exists(dir))
+        {
+            Console.WriteLine($"no such directory: {dir}");
+            return;
+        }
+
+        if (File.Exists(configPath))
+        {
+            //todo: read file and execute
+        }
+        else
+        {
+            //todo: create file and set default value
+            File.Create(configPath);
+            var env = new EnvConfig(dir);
+            var json = JsonSerializer.Serialize(env);
+            File.WriteAllText(configPath, json);
+        }
+    }
+}
